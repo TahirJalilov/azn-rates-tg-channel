6f831ac2f752f60be8be5222c9cc33c03887db76
Tahir Jalilov <tahir.jalilov@gmail.com>
2020-11-22 11:50:09 +0400

    product (from itertools) is used instead of for in for

--- cbar.py
+++ cbar.py
@@ -2,6 +2,7 @@
 import requests
 import xmltodict
 from datetime import datetime, timedelta
+from itertools import product
 
 
 def get_xml_from_cbar(date: datetime):
@@ -20,12 +21,12 @@ def cbar_data_by_date(date):
 def cbar_data_with_difference():
     cbar_data_today = cbar_data_by_date(datetime.today())
     cbar_data_yesterday = cbar_data_by_date(datetime.today() - timedelta(days=1))
-    for currency_today in cbar_data_today["ValCurs"]["ValType"][1]["Valute"]:
-        for currency_yesterday in cbar_data_yesterday["ValCurs"]["ValType"][1][
-            "Valute"
-        ]:
-            if currency_today["@Code"] == currency_yesterday["@Code"]:
-                currency_today["difference"] = float(currency_today["Value"]) - float(
-                    currency_yesterday["Value"]
-                )
+    for currency_today, currency_yesterday in product(
+        cbar_data_today["ValCurs"]["ValType"][1]["Valute"],
+        cbar_data_yesterday["ValCurs"]["ValType"][1]["Valute"],
+    ):
+        if currency_today["@Code"] == currency_yesterday["@Code"]:
+            currency_today["difference"] = float(currency_today["Value"]) - float(
+                currency_yesterday["Value"]
+            )
     return cbar_data_today
