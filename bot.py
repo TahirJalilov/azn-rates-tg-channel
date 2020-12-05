5d5f11402d9da9ebf62288d27b34186128ccc825
Tahir Jalilov <tahir.jalilov@gmail.com>
2020-12-05 13:05:41 +0400

    code refactored

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+
 import requests
-from config import CHANNEL, BOT_TOKEN
+
+from config import BOT_TOKEN, CHANNEL
 
 
 def post_message(text):
     requests.get(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        params=dict(
-            chat_id=CHANNEL, text=text, parse_mode="HTML", disable_web_page_preview=True
-        ),
+        'https://api.telegram.org/bot{0}/sendMessage'.format(BOT_TOKEN),
+        params={
+            'chat_id': CHANNEL,
+            'text': text,
+            'parse_mode': 'HTML',
+            'disable_web_page_preview': True,
+        },
     )
