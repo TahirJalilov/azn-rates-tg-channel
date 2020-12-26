24dfccd3fdd9dced348ff6600852d8834a521cf4
Tahir Jalilov <tahir.jalilov@gmail.com>
2020-12-26 23:18:56 +0400

    code refactored

--- cbar.py
+++ cbar.py
@@ -1,45 +1,66 @@
 # -*- coding: utf-8 -*-
+"""Module for work with rates xml."""
+
 from datetime import datetime, timedelta
-from itertools import product
 from typing import Union
 
 import requests
 import xmltodict
 
 
-def get_xml_from_cbar(date: datetime) -> Union[str, bool]:
+def _get_xml_with_rates(date: datetime) -> Union[str, bool]:
+    """Get xml file from cbar web page.
+
+    Args:
+        date: date of rates
+
+    Returns:
+        xml file as a text or False
+    """
     response = requests.get(
         'https://www.cbar.az/currencies/{0}.xml'.format(
-            date.strftime('%d.%m.%Y')),
+            date.strftime('%d.%m.%Y'),
+        ),
     )
     if not response.text.startswith('<?xml'):
         return False
     return response.text
 
 
-def cbar_data_by_date(date: datetime) -> Union[dict, bool]:
-    xml = get_xml_from_cbar(date)
+def currency_rates_by_date(date: datetime) -> Union[dict, bool]:
+    """Get dictionary with rates.
+
+    Args:
+        date: date of rates
+
+    Returns:
+        dict with rates or False
+    """
+    xml = _get_xml_with_rates(date)
     try:
-        cbar_data = xmltodict.parse(xml)
+        currency_rates = xmltodict.parse(xml)
     except TypeError:
         return False
     else:
-        return cbar_data
+        return currency_rates['ValCurs']
 
 
-def cbar_data_with_difference() -> Union[dict, bool]:
-    cbar_data_today = cbar_data_by_date(datetime.today())
-    cbar_data_yesterday = cbar_data_by_date(
+def currency_rates_with_diff() -> Union[dict, bool]:
+    """Get dictionary with rates and calculated differences.
+
+    Returns:
+        dict with rates and calculated differences or False
+    """
+    rates_today = currency_rates_by_date(datetime.today())
+    rates_yesterday = currency_rates_by_date(
         datetime.today() - timedelta(days=1),
     )
-    if cbar_data_today['ValCurs']['@Date'] == cbar_data_yesterday['ValCurs']['@Date']:
+    if rates_today['@Date'] == rates_yesterday['@Date']:
         return False
-    for currency_today, currency_yesterday in product(
-        cbar_data_today['ValCurs']['ValType'][1]['Valute'],
-        cbar_data_yesterday['ValCurs']['ValType'][1]['Valute'],
+    for rates in zip(
+        rates_today['ValType'][1]['Valute'],
+        rates_yesterday['ValType'][1]['Valute'],
     ):
-        if currency_today['@Code'] == currency_yesterday['@Code']:
-            currency_today['difference'] = float(currency_today['Value']) - float(
-                currency_yesterday['Value'],
-            )
-    return cbar_data_today
+        rates[0]['diff'] = float(rates[0]['Value']) - float(rates[1]['Value'])
+
+    return rates_today
