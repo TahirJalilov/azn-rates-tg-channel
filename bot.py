bfd68e4725c8e8e67a00eb573adec8d884586940
Tahir Jalilov <tahir.jalilov@gmail.com>
2020-12-26 11:58:41 +0400

    code refactoring

--- bot.py
+++ bot.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
+"""Module for working with bot."""
 
 import requests
+import toml
 
-from config import BOT_TOKEN, CHANNEL
+config = toml.load('config.toml')
+token = config['bot']['token']
 
 
-def post_message(text):
+def send_message(text: str) -> None:
+    """Send message to the channel.
+
+    Args:
+        text: text of message
+    """
     requests.get(
-        'https://api.telegram.org/bot{0}/sendMessage'.format(BOT_TOKEN),
+        'https://api.telegram.org/bot{0}/sendMessage'.format(token),
         params={
-            'chat_id': CHANNEL,
+            'chat_id': config['channel']['name'],
             'text': text,
             'parse_mode': 'HTML',
             'disable_web_page_preview': True,
