e8375d0c0c64d66625974506cde6dd41a76d2927
Tahir Jalilov <tahir.jalilov@gmail.com>
2021-03-17 19:21:48 +0400

    first init

---
+++ bot.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Module for working with bot."""
+
+import requests
+
+from config import BOT_TOKEN, TG_CHANNELS
+
+
+def send_message(text: str) -> None:
+    """Send message to the channel.
+
+    Args:
+        text: text of message
+    """
+    for chat_id in TG_CHANNELS:
+        requests.get(
+            'https://api.telegram.org/bot{0}/sendMessage'.format(BOT_TOKEN),
+            params={
+                'chat_id': chat_id,
+                'text': text,
+                'parse_mode': 'HTML',
+                'disable_web_page_preview': True,
+            },
+        )
