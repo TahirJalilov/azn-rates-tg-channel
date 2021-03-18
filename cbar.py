01fd7306f44060828d701dbbfc1031a731c8fa5c
Tahir Jalilov <tahir.jalilov@gmail.com>
2021-03-18 19:22:09 +0400

    cbar file added

---
+++ cbar.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Module for work with rates xml."""
+
+from datetime import datetime, timedelta
+from typing import Union
+
+import requests
+import xmltodict
+
+
+def _get_xml_with_rates(date: datetime) -> Union[str, bool]:
+    """Get xml file from cbar web page.
+
+    Args:
+        date: date of rates
+
+    Returns:
+        xml file as a text or False
+    """
+    response = requests.get(
+        'https://www.cbar.az/currencies/{0}.xml'.format(
+            date.strftime('%d.%m.%Y'),
+        ),
+    )
+    if not response.text.startswith('<?xml'):
+        return False
+    return response.text
+
+
+def currency_rates_by_date(date: datetime) -> Union[dict, bool]:
+    """Get dictionary with rates.
+
+    Args:
+        date: date of rates
+
+    Returns:
+        dict with rates or False
+    """
+    xml = _get_xml_with_rates(date)
+    try:
+        currency_rates = xmltodict.parse(xml)
+    except TypeError:
+        return False
+    else:
+        return currency_rates['ValCurs']
+
+
+def currency_rates_with_diff() -> Union[dict, bool]:
+    """Get dictionary with rates and calculated differences.
+
+    Returns:
+        dict with rates and calculated differences or False
+    """
+    rates_today = currency_rates_by_date(datetime.today())
+    rates_yesterday = currency_rates_by_date(
+        datetime.today() - timedelta(days=1),
+    )
+    if rates_today['@Date'] == rates_yesterday['@Date']:
+        return False
+    for rates in zip(
+        rates_today['ValType'][1]['Valute'],
+        rates_yesterday['ValType'][1]['Valute'],
+    ):
+        rates[0]['diff'] = float(rates[0]['Value']) - float(rates[1]['Value'])
+
+    return rates_today
